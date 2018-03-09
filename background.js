fed202b8284d3891a11944d6957d848093f0c4d6
Keertana Chandar <kchandar@uw.edu>
2018-03-09 15:33:03 -0800

    working on getting warnings after 10 min

--- background.js
+++ background.js
@@ -1,5 +1,6 @@
 var currentDomain = ""
 let indexToDayOfWeek = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"]
+var startTime = null
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     let date = new Date()
@@ -13,10 +14,18 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
         let domain = new URL(tab.url).hostname
         if (domain != currentDomain) {
             currentDomain = domain
-            if (localStorage.sites) {
-                let value = sites[domain]
-                if (value && value[indexToDayOfWeek[dow]]) {
-                    alert(`Are you sure you want to spend time at ${domain}???`)
+            let value = sites[domain]
+            if (value && value[indexToDayOfWeek[dow]]) {
+                alert(`Are you sure you want to spend time at ${domain}???`)
+            }
+        } else if (domain == currentDomain) {
+            let value = sites[domain]
+            if (value && value[indexToDayOfWeek[dow]]) {
+                let start = startTime.getTime()
+                let curr = date.getTime()
+                // if(curr - start >= 600000) {
+                if (curr - start >= 10000) {
+                    alert(`You have spent over 10 minutes on this site since the last warning. `)
                 }
             }
         }
@@ -38,7 +47,21 @@ chrome.tabs.onActivated.addListener((activeInfo) => {
             let value = sites[domain]
             if (value && value[indexToDayOfWeek[dow]]) {
                 alert(`Are you sure you want to spend time at ${domain}???`)
+                startTime = date
             }
         }
     })
 })
+
+// function updateTime(site, seconds){
+//     if (!localStorage.sites) {
+//        localStorage.sites = JSON.stringify({});
+//      }
+
+//       var sites = JSON.parse(localStorage.sites);
+//      if (!sites[site]) {
+//        sites[site] = 0;
+//      }
+//      sites[site] = sites[site] + seconds;
+//      localStorage.sites = JSON.stringify(sites);  
+//  }
