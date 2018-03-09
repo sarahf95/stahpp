22477a30cb82a38f3334ae2961476e6bf73e4c71
sarahfeldmann <sarahf95@gmail.com>
2018-03-08 20:26:23 -0800

    finish barebone functionality

--- popup.js
+++ popup.js
@@ -14,42 +14,65 @@ function getCurrentTabUrl(callback) {
 }
 
 function getSavedStatus(domain, callback) {
-  if(!localStorage.sites) {
+  if (!localStorage.sites) {
     callback(null)
   } else {
     var sites = JSON.parse(localStorage.sites)
-    if(!sites[domain]) {
+    if (!sites[domain]) {
       callback(null)
     } else {
       callback(sites[domain])
     }
   }
-  
+
 }
 
 document.addEventListener('DOMContentLoaded', () => {
   getCurrentTabUrl((domain) => {
     var button = document.getElementById('actionButton');
+    button.innerHTML = "Set Warning"
     document.getElementById('currentSite').innerHTML = domain
+    button.setAttribute("class", "addWarning")
+
     getSavedStatus(domain, (status) => {
       if (status) {
-        console.log("it was saved!");
+        button.innerHTML = "Disable Warnings";
+        button.setAttribute("class", "removeWarning")
+      } else {
+        button.innerHTML = "Set Warnings";
+        button.setAttribute("class", "addWarning")
       }
     });
     button.addEventListener('click', () => {
-      if(!localStorage.sites) {
+      if (!localStorage.sites) {
         localStorage.sites = JSON.stringify({})
       }
       var sites = JSON.parse(localStorage.sites);
-      if(!sites[domain]) {
+      if (!sites[domain]) {
         //{"mon":true, "tue":true, "wed":true, "thu":true, "fri":true, "sat":true, "sun":true}
         sites[domain] = true;
-        localStorage.sites = JSON.stringify(sites); 
-        console.log("saved");
+        localStorage.sites = JSON.stringify(sites);
+        button.setAttribute("disabled", "disabled")
+        button.innerHTML = "Saved"
+        button.setAttribute("class", "saveWarning")
+
       } else {
-        console.log("already saved");
+        button.innerHTML = "Disable Warnings";
+        button.setAttribute("class", "removeWarning")
+        removeWarning(domain)
       }
 
     });
   });
 });
+
+function removeWarning(domain) {
+  if (localStorage.sites) {
+    var sites = JSON.parse(localStorage.sites);
+    if (sites[domain]) {
+      delete sites[domain]
+      localStorage.sites = JSON.stringify(sites);
+      alert("You've disabled the warning")
+    }
+  }
+}
