cc32f7c5c44a05d420c15538f933f92389fbdb96
Keertana Chandar <kchandar@uw.edu>
2018-03-08 17:59:00 -0800

    progress

--- background.js
+++ background.js
@@ -1,57 +1,52 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    let domain = changeInfo.url
-    if (localStorage.sites) {
-        var sites = JSON.parse(localStorage.sites);
-        if (sites[domain]) {
-            alert(`Are you sure you want to spend time at ${sites[domain]}`)
-            console.log(`Are you sure you want to spend time at ${sites[domain]}`)
-
-        }
+    if (!localStorage.logs) {
+        localStorage.logs = JSON.stringify({})
     }
     console.log("in onUpdated")
+    chrome.tabs.get(changeInfo.tabId, (tab) => {
+        let domain = new URL(tab.url).hostname
+        if (localStorage.sites) {
+            var sites = JSON.parse(localStorage.sites);
+            if (sites[domain]) {
+                alert(`Are you sure you want to spend time at ${domain}`)
+            }
+        }
+    })
 })
 
 
-chrome.tabs.onCreated.addListener((tabId, moveInfo) => {
+chrome.tabs.onCreated.addListener((tab) => {
     if (!localStorage.logs) {
         localStorage.logs = JSON.stringify({})
     }
     console.log("in onCreated")
-    var logs = JSON.parse(localStorage.logs);
-    logs["onCreated"] = ""
-    localStorage.logs = JSON.stringify(logs);
-})
-
-chrome.tabs.onMoved.addListener((tab) => {
-
-    if (!localStorage.logs) {
-        localStorage.logs = JSON.stringify({})
+    let domain = new URL(tab.url).hostname
+    if (localStorage.sites) {
+        var sites = JSON.parse(localStorage.sites);
+        if (sites[domain]) {
+            alert(`Are you sure you want to spend time at ${domain}`)
+        }
     }
-    console.log("in onMoved")
-    var logs = JSON.parse(localStorage.logs);
-    logs["onMoved"] = ""
-    localStorage.logs = JSON.stringify(logs);
 })
 
 chrome.tabs.onActivated.addListener((activeInfo) => {
     if (!localStorage.logs) {
         localStorage.logs = JSON.stringify({})
     }
-    var logs = JSON.parse(localStorage.logs);
-    logs["actiaveted"] = activeInfo.tabId
-    localStorage.logs = JSON.stringify(logs);
-    alert(localStorage.logs);
-    console.log("in onActivated")
-    var logs = JSON.parse(localStorage.logs);
-    logs["onActivated"] = ""
+    // var logs = JSON.parse(localStorage.logs);
+    // logs["actiaveted"] = activeInfo.tabId
+    // localStorage.logs = JSON.stringify(logs);
+    // console.log("in onActivated")
+    // var logs = JSON.parse(localStorage.logs);
+    // logs["onActivated"] = ""
+    // logs["domainActivated"] = domain
+    // localStorage.logs = JSON.stringify(logs);
     chrome.tabs.get(activeInfo.tabId, (tab) => {
         let domain = new URL(tab.url).hostname
-        logs["domainActivated"] = domain
-        localStorage.logs = JSON.stringify(logs);
         if (localStorage.sites) {
             var sites = JSON.parse(localStorage.sites);
             if (sites[domain]) {
-                alert(`Are you sure you want to spend time at ${sites[domain]}`)
+                alert(`Are you sure you want to spend time at ${domain}`)
             }
         }
     })
@@ -63,25 +58,19 @@ chrome.tabs.onHighlighted.addListener((highlightInfo) => {
         localStorage.logs = JSON.stringify({})
     }
     console.log("in onHighlighted")
-    var logs = JSON.parse(localStorage.logs);
-    logs["onHighlighted"] = ""
-    localStorage.logs = JSON.stringify(logs);
-    chrome.tabs.get(tabId, (tab) => {
+    chrome.tabs.get(highlightInfo.tabIds[0], (tab) => {
         let domain = new URL(tab.url).hostname
-        console.log(domain)
-        var logs = JSON.parse(localStorage.logs);
-        logs["domainHighlighted"] = domain
-        localStorage.logs = JSON.stringify(logs);
         if (localStorage.sites) {
             var sites = JSON.parse(localStorage.sites);
             if (sites[domain]) {
-                alert(`Are you sure you want to spend time at ${sites[domain]}`)
-                console.log(`Are you sure you want to spend time at ${sites[domain]}`)
-
+                alert(`Are you sure you want to spend time at ${domain}`)
             }
         }
     })
 })
+
+
+///////
 
 chrome.tabs.onDetached.addListener((tabId, detachInfo) => {
 
@@ -135,5 +124,16 @@ chrome.tabs.onZoomChange.addListener((ZoomChangeInfo) => {
     console.log("in onZoomChange")
     var logs = JSON.parse(localStorage.logs);
     logs["onZoomChange"] = ""
+    localStorage.logs = JSON.stringify(logs);
+})
+
+chrome.tabs.onMoved.addListener((tab) => {
+
+    if (!localStorage.logs) {
+        localStorage.logs = JSON.stringify({})
+    }
+    console.log("in onMoved")
+    var logs = JSON.parse(localStorage.logs);
+    logs["onMoved"] = ""
     localStorage.logs = JSON.stringify(logs);
 })
